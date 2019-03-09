1f1ae3345955fb1d6a7509df995bbbe82df1a478
zifeng <zifeng@localhost.localdomain>
2019-03-09 17:05:25 +0800

    fix export_embeddings.py

--- src/export_embedings.py
+++ src/export_embedings.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import tensorflow as tf
+"""
+use pre-trained `Inception Resnet v1` as a image feature extractor.
+refer to: https://github.com/davidsandberg/facenet
+"""
+import sys
 import os
 import pdb
 import re
-import glob
-import cv2
+import argparse
+import time
+
+import tensorflow as tf
 import numpy as np
-# from tensorflow.python import pywrap_tensorflow
+from keras.preprocessing.image import ImageDataGenerator
 
 # force using CPU
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
 
 def get_model_filenames(model_dir):
     files = os.listdir(model_dir)
@@ -38,55 +43,66 @@ def get_model_filenames(model_dir):
                 ckpt_file = step_str.groups()[0]
     return meta_file, ckpt_file
 
-# obtain embedding
-with tf.Graph().as_default():
-    with tf.Session() as sess:
 
-        # load model
-        model_path = os.path.join(os.getcwd(),"model/20180402-114759")
-        meta_file,ckpt_file = get_model_filenames(model_path)
-        saver = tf.train.import_meta_graph(os.path.join(model_path,meta_file))
-        saver.restore(tf.get_default_session(),os.path.join(model_path,ckpt_file))
-        
-        image_size = 160
-        
-        # Get input and output tensors
-        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-        print("embedding loaded:",embeddings)
-        
-        # load images
-        images = glob.glob("faces/*.png")
-        img_ar = []
-        for img in images:
-            img_ = cv2.imread(img,cv2.IMREAD_COLOR)
-            img_ = cv2.resize(img_,(image_size,image_size))
-            img_ = cv2.cvtColor(img_,cv2.COLOR_BGR2RGB)
-            img_ar.append(img_)
+def main(args):
+    # obtain embedding
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            # load model
+            model_path = os.path.join(os.getcwd(),"model/20180402-114759")
+            meta_file,ckpt_file = get_model_filenames(model_path)
+            saver = tf.train.import_meta_graph(os.path.join(model_path,meta_file))
+            saver.restore(tf.get_default_session(),os.path.join(model_path,ckpt_file))
+            image_size = 160
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            print("embedding loaded:",embeddings)
             
-        img_ar = np.array(img_ar)
-        
-        # obtain embeddings
-        feed_dict = {images_placeholder:img_ar,phase_train_placeholder:False}
-        res = sess.run(embeddings,feed_dict=feed_dict)
-        
-        print("get embeddings with shape:",res.shape)
-        
-        # training
-
-        """
-        # print tensors value and name
-        reader = pywrap_tensorflow.NewCheckpointReader(os.path.join(model_path,ckpt_file))
-        var_to_shape_map = reader.get_variable_to_shape_map()
-        for key in var_to_shape_map:
-            print("tensor name",key)
-            # print(reader.get_tensor(key))
-        """
-        """
-        # plot graph
-        writer = tf.summary.FileWriter("summary/log.log",sess.graph)
-        writer.close()
-        """
+            # image generator
+            imagen = ImageDataGenerator(rescale=1./255)
+            imgs = imagen.flow_from_directory(args.input_dir,
+                target_size=(image_size,image_size),
+                batch_size = args.batch_size,
+                shuffle=False,
+                color_mode = "rgb",
+                class_mode = None,
+                classes=None,
+                )
 
 
+            # load images
+            for i,img in enumerate(imgs):
+                # pdb.set_trace()
+                feed_dict = {images_placeholder:img,phase_train_placeholder:False}
+                res = sess.run(embeddings,feed_dict=feed_dict)
+                if i == 0:
+                    emb_ar = res
+                else:
+                    emb_ar = np.r_[emb_ar,res]
+                print("batch",i,"get embeddings with shape",res.shape)
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    np.save(os.path.join(args.output_dir,
+        "embdata_%s.npy"%(time.strftime("%Y%m%d_%H_%M_%S"))),emb_ar)
+
+    print("export complete!")
+        
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir",type=str,
+            help="input images directory.")
+    parser.add_argument("--output_dir",type=str,
+            default="./data",
+            help="output image embeddings dir")
+    parser.add_argument("--batch_size",type=int,
+            default="100",
+            help="batch size of the input images.")
+
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
